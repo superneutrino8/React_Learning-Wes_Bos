0168f5271441b6fbc719425530c9c32f132b4caa
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-04-29 08:44:02 +0530

    learning

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>----</p>
+                <p>Hello World!</p>
+                <p>----</p>
+            </React.Fragment>
+        );
+    }
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
 // let's go!
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));

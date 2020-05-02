37e0d00b099767e18b4db1d2be08bf841a7c51b9
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-05-02 10:20:10 +0530

    minor changes

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+import './css/style.css';
+
 import StorePicker from './components/StorePicker';
 
 ReactDOM.render(<StorePicker/>, document.querySelector('#main'));

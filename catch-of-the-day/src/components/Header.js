9f675cc757450476ca2b9da17213787a24f1e5b8
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-05-04 13:15:08 +0530

    changed header component to function component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
-    state = {}
-    render() {
-        return (
-            <header className="top">
-                <h1>Catch 
-                    <span className="ofThe">
+const Header = props => {
+    return (
+        <header className="top">
+            <h1>Catch
+                <span className="ofThe">
                     <span className="of">Of</span>
-                    <span className="the">The</span> 
-                    </span>
-                    Day
-                </h1>
-                <h3 className="tagline">
-                    <span>Fresh Seafood Market</span>
-                </h3>
-            </header>
-        );
-    }
-}
+                    <span className="the">The</span>
+                </span>
+                Day
+            </h1>
+            <h3 className="tagline">
+                <span>{props.tagline}</span>
+            </h3>
+        </header>
+    );
+};
 
 export default Header;
